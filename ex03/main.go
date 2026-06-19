2d1c026b16b5a4bd9eaf82512abf8b054e26b87f
maxchichar <maxwellchibuezecharles@gmail.com>
2026-06-19 17:34:00 +0100

    Built a server with a /count route. If a user sends a GET request, return the text 'Send a POST request with text to count words'. If they send a POST request, read the text body and return the number of characters.

---
+++ ex03/main.go
@@ -0,0 +1,27 @@
+package main
+
+import(
+	"io"
+	"log"
+	"fmt"
+	"net/http"
+)
+
+func countHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET"{
+		fmt.Fprint(w, "Send a POST request with text to count words")
+	}else if r.Method == "POST"{
+		body, err := io.ReadAll(r.Body)
+		if err != nil{
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(w, "%d", len(body)) // calculate the length of the 
+	}
+}
+
+func main() {
+	http.HandleFunc("/count", countHandler)
+	fmt.Println("Server running at http://localhost:8080/count")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}

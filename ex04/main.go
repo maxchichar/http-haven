992df041b02cb51e15bca70ee913777108408412
maxchichar <maxwellchibuezecharles@gmail.com>
2026-06-20 19:38:00 +0100

    Created a /calculate route that accepts three query parameters: op (operation), a, and b. For example, /calculate?op=add&a=10&b=5 should respond with Result: 15.

--- ex04/main.go
+++ ex04/main.go
@@ -8,29 +8,32 @@ import (
 )
 
 func calculateHandler(w http.ResponseWriter, r *http.Request) {
+	// Peformed method check that wil return HTTP error if page not found
 	if r.Method != "GET"{
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
 
 	op := r.URL.Query().Get("op")
-
+	
+	// Conversion of string to integer using the Atoi in other to perform Arithemetic calculations
 	a := r.URL.Query().Get("a")
 	A, err := strconv.Atoi(a)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest) // This return HTTP error 400
 		return
 	}
 
 	b := r.URL.Query().Get("b")
-	B, err := strconv.Atoi(b)
+	B, err := strconv.Atoi(b) 
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	var result int
+	var result int // This stores the output data
 
+	// Arithemetic Formula
 	switch op {
 	case "add":
 		result = A + B
@@ -39,15 +42,15 @@ func calculateHandler(w http.ResponseWriter, r *http.Request) {
 	case "multiply":
 		result = A * B
 	default:
-		w.WriteHeader(http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest) // Returns error if the operator is unknown
 		return
 	}
 
-	fmt.Fprintf(w, "Result: %d", result)
+	fmt.Fprintf(w, "Result: %d", result) // What the user see as the server output
 }
 
 func main()  {
 	http.HandleFunc("/calculate", calculateHandler)
-	fmt.Println("Server running at http://localhost:8080/calculate")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Server running at http://localhost:8080/calculate") // The URL to perform you're calculations
+	log.Fatal(http.ListenAndServe(":8080", nil)) // Server Address where it is hosted at :8080
 }
